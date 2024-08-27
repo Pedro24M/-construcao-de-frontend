892471fbce6329064a13a3b82b6beba1ebbac55c
Pedro24M <exacadrillex@gmail.com>
2024-08-27 20:33:59 -0300

    Aula03

---
+++ aulas/aula03/src/App.jsx
@@ -0,0 +1,13 @@
+import Titulo from "./assets/components/titulo";
+
+function App() {
+  return (
+    <div>
+      {console.log("Olá")}
+      <Titulo />
+      <p>Mina Aplicação</p>
+    </div>
+  )
+}
+
+export default App;
