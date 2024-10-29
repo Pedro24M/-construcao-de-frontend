6a9f88aa010e03e7c758b80249d3396f551ea215
Pedro24M <exacadrillex@gmail.com>
2024-10-29 19:33:24 -0300

    Aula10 Quase Completa

---
+++ aulas/aula10/front/src/pages/Editar.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Cabecalho from "../components/Cabecalho";
+import Conteudo from "../components/Conteudo";
+import Formulario from "./Formulario";
+import { obterContato } from "../services/ContatoService";
+
+function Editar() {
+  const { id } = useParams();
+  const [contato, setContato] = useState({});
+  const [erro, setErro] = useState();
+
+  const carregar = async () => {
+    const resultado = await obterContato(id);
+    if (resultado.sucesso) {
+      console.log(resultado.dados)
+      setContato(resultado.dados);
+      setErro("");
+    } else {
+      setErro(resultado.mensagem);
+    }
+  };
+
+  useEffect(() => {
+    carregar();
+  }, []);
+
+  return (
+    <>
+      <Cabecalho />
+      <Conteudo>
+        {erro && <p>{erro}</p>}
+        <h2>Editar Contato</h2>
+        <Formulario dados={contato} trataEnviar={() => {}} />
+      </Conteudo>
+    </>
+  );
+}
+
+export default Editar;
---
+++ aulas/aula10/front/src/services/ContatoService.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const url = import.meta.env.VITE_URL_API;
+
+function carregarContatos() {
+  return axios
+    .get(url)
+    .then((response) => {
+      return { sucesso: true, dados: response.data };
+    })
+    .catch((error) => {
+      return { sucesso: false, mensagem: error.message };
+    });
+}
+
+function criarContato(contato) {
+  return axios
+    .post(url, contato)
+    .then((response) => {
+      return { sucesso: true, dados: response.data };
+    })
+    .catch((error) => {
+      return { sucesso: false, mensagem: error.message };
+    });
+
+  // try {
+  //   const response = await axios.post(url, contato)
+  //   return { sucesso: true, dados: response.data }
+  // } catch (error)  {
+  //   return { sucesso: false, mensagem: error.message }
+  // }
+}
+
+function atualizarContato(contato) {}
+
+function removerContato(id) {
+  return axios
+    .delete(`${url}/${id}`)
+    .then((response) => {
+      return { sucesso: true, dados: response.data };
+    })
+    .catch((error) => {
+      return { sucesso: false, mensagem: error.message };
+    });
+}
+
+function obterContato(id) {
+  return axios
+  .get(`${url}/${id}`)
+  .then((response) => {
+    return {sucesso: true, dados: response.data};
+  })
+  .catch((error) => {
+    return { sucesso: false, mensagem: error.message };
+  });
+}
+
+export { carregarContatos, criarContato, removerContato, obterContato };
